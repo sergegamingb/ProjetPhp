1740a78f27b1727f4aeea69a531ec2432facec13
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-03 12:23:07 +0200

    bd

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -4,6 +4,49 @@ include 'base.php';
 
 class models_user
 {
+    public function addUser($dbLink)
+    {
 
+        $action = $_POST['action'];
+        $email = $_POST['mail'];
+        $password = $_POST['mdp'];
+        $identifiant = $_POST['identifiant'];
+        $sexe = $_POST['genre'];
+        $telephone = $_POST['phone'];
+        $pays = $_POST['pays'];
+        $conditions = 'member';
+
+        if ($action == 'submit') {
+            $query = 'INSERT INTO USER (mail, pseudo, password, phone, country, user_date, gender, state) 
+        VALUES ( \'' . $email . '\' ,
+         \'' . $identifiant . '\',
+         \'' . $password . '\' ,
+         \'' . $telephone . '\' ,
+         \'' . $pays . '\' ,
+         NOW(),
+         \'' . $sexe . '\' ,
+         \'' . $conditions . '\'
+         )';
+
+
+            if (!($dbResult = mysqli_query($dbLink, $query))) {
+                echo 'Erreur dans requête<br />';
+// Affiche le type d'erreur.
+                echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+                echo 'Requête : ' . $query . '<br/>';
+                exit();
+            } else {
+                echo '<br/><strong>bonsoir, votre inscription a bien été enregistrée.</strong><br/>';
+                echo '<br/><strong>Mail envoyé !</strong><br/>';
+            }
+        } else
+            echo '<br/><strong>Bouton non géré !</strong><br/>';
+        end_page();
+    }
 
 }
+?>
+
+
+
