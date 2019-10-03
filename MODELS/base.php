e300ea6cdd0693c5cd1b559573cc02f6eaab7540
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-03 11:38:23 +0200

    connexion BD

--- MODELS/base.php
+++ MODELS/base.php
@@ -6,7 +6,7 @@ include 'start_end.php';
     $dbHost = 'mysql-tpphp.alwaysdata.net';
     $dbUser = 'tpphp';
     $pwd = 'EricZemour';
-    $dbName = 'tpphp_db';
+    $dbName = 'tpphp_bd';
     $dbLink = mysqli_connect($dbHost, $dbUser, $pwd)
     or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
