af5d7bcb2c052546a86c04cfde25d7887068afec
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-07 13:11:02 +0200

    Merge branch 'master' of /amuhome/s18003992/Documents/ProjetPhp with conflicts.

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -3,22 +3,23 @@
 
 include 'base.php';
 
+class User
+{
+    private $dbLink;
+    private $action;
+    private $email;
+    private $password;
+    private $identifiant;
+    private $sexe;
+    private $telephone;
+    private $pays;
+    private $conditions;
 
-     function addUser()
+    public function __construct()
     {
 
-        $dbLink=$GLOBALS['dbLink'];
-        $action = $_POST['action'];
-        $email = $_POST['mail'];
-        $password = $_POST['mdp'];
-        $identifiant = $_POST['identifiant'];
-        $sexe = $_POST['genre'];
-        $telephone = $_POST['phone'];
-        $pays = $_POST['pays'];
-        $conditions = 'member';
 
-        if ($action == 'inscription') {
-            $query = 'INSERT INTO USER (mail, pseudo, password, phone, country, user_date, gender, state) 
+        $query = 'INSERT INTO USER (mail, pseudo, password, phone, country, user_date, gender, state) 
         VALUES ( \'' . $email . '\' ,
          \'' . $identifiant . '\',
          \'' . $password . '\' ,
@@ -30,22 +31,24 @@ include 'base.php';
          )';
 
 
-            if (!($dbResult = mysqli_query($dbLink, $query))) {
-                echo 'Erreur dans requête<br />';
+        if (!($dbResult = mysqli_query($dbLink, $query))) {
+            echo 'Erreur dans requête<br />';
 // Affiche le type d'erreur.
-                echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
 // Affiche la requête envoyée.
-                echo 'Requête : ' . $query . '<br/>';
-                exit();
-            } else {
-                echo '<br/><strong>bonsoir, votre inscription a bien été enregistrée.</strong><br/>';
-                echo '<br/><strong>Mail envoyé !</strong><br/>';
-            }
-        } else
-            echo '<br/><strong>Bouton non géré !</strong><br/>';
-        end_page();
+            echo 'Requête : ' . $query . '<br/>';
+            exit();
+        } else {
+            echo '<br/><strong>bonsoir, votre inscription a bien été enregistrée.</strong><br/>';
+            echo '<br/><strong>Mail envoyé !</strong><br/>';
+        }
+    }
 
 }
+
+
+
+
 ?>
 
 
