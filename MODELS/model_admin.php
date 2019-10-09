3f016a6e29015352aa58d53ce1758cfaabcb5050
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-10-09 17:11:05 +0200

    ajout classe admin

---
+++ MODELS/model_admin.php
@@ -0,0 +1,64 @@
+<?php
+
+include 'model_base.php';
+
+class Admin extends User
+{
+
+public function __construct()
+{
+
+}
+
+public function deleteMessage($message_id)
+{
+
+}
+
+public function deleteDiscussion($discussion_id)
+{
+
+}
+
+public function deleteUser($user_id)
+{
+
+}
+
+public function updateMessage($message_id)
+{
+
+}
+
+public function updateDiscussion($discussion_id)
+{
+
+}
+
+public function updateUser($user_id)
+{
+
+}
+
+public function createMessage($message_id)
+{
+
+}
+
+public function createDiscussion($discussion_id)
+{
+
+}
+
+public function readMessage($message_id)
+{
+
+}
+
+public function readDiscussion($discussion_id)
+{
+
+}
+}
+
+?>
