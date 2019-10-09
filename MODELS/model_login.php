f9af2a8ee6c0e60c9645854cdc8c634fd6354510
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-09 17:03:53 +0200

    bd

--- MODELS/model_login.php
+++ MODELS/model_login.php
@@ -1,18 +1,36 @@
 <?php
-include 'start_end.php';
+include 'mdel_base.php';
 start_page('login');
-?>
 
-    <form action="test-pass.php" method="post">
+$login=$_POST['login'];
+$password=$_POST['mdp'];
+$query = "SELECT identifiant,password FROM user WHERE identifiant='$login' and password='$password'";
+if (!($dbResult = mysqli_query($dbLink, $query))) {
+    echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+if($dbRow = mysqli_fetch_assoc($dbResult))
+{
+    header('Location:view_index.php');
+}
+else
+{
+    header('Location:view_index.php');
+}
 
-        <label for="email">E-mail :</label><input id="Email" type="text" name="Email"><br/>
 
-        <label for="Password"Mot de passe :</label><input id="Password" type="password" name="Password"><br/>
 
-        <input id="action" type="submit" name="action" value="Connexion"><br/>
 
-    </form>
 
-<?php
+
+
+
+
+
+
 end_page();
 ?>
