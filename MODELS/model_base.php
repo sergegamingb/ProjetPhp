781e087ba7e980430b33685b5fc3e860783692b9
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-10-10 11:10:08 +0200

    PDO

--- MODELS/model_base.php
+++ MODELS/model_base.php
@@ -5,17 +5,8 @@ include 'model_start_end.php';
 
 
 
-    $dbHost = 'mysql-tpphp.alwaysdata.net';
-    $dbUser = 'tpphp';
-    $pwd = 'ericzemour';
+    $db = new PDO('mysql:host=mysql-tpphp.alwaysdata.net;dbname=tpphp_bd', 'tpphp', 'ericzemour');
 
-
-    $dbName = 'tpphp_bd';
-    $dbLink = mysqli_connect($dbHost, $dbUser, $pwd)
-    or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-
-    mysqli_select_db($dbLink, $dbName)
-    or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 
 
 
