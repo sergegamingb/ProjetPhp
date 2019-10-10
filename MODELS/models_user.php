b3ce6fa0684f6974343ced0b3d904c7d24aa9862
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-10 14:05:50 +0200

    test login

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -216,6 +216,40 @@ class User
 
         }
     }
+
+    public function login()
+    {
+
+        session_start();
+
+        $login = $_POST['login'];
+        $password = $_POST['mdp'];
+        $dbLink = $GLOBALS['dbLink'];
+
+
+        $query = "SELECT pseudo , password  FROM USER where USER.pseudo =  '$login'  and USER.password = '$password' ";
+        if (!($dbResult = mysqli_query($dbLink, $query))) {
+            echo 'Erreur de requête<br/>';
+// Affiche le type d'erreur.
+            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+// Affiche la requête envoyée.
+            echo 'Requête : ' . $query . '<br/>';
+            exit();
+        }
+        if($dbRow = mysqli_fetch_assoc($dbResult))
+        {
+            $_SESSION['isLogin']='ok';
+            $_SESSION['login']=$login;
+            $_SESSION['password']=$password;
+            header('Location: ../index.php');
+        }
+
+        else
+        {
+            echo 'erreur d\'authentification';
+            echo '<br/> <a href="../VIEWS/view_index.html"> retourner au login</a> ';
+        }
+    }
 }
 
 
