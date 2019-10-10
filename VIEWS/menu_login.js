9bcb99bb886bc62c0062d140113911cd7f219edf
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-10 11:23:51 +0200

    bd

--- VIEWS/menu_login.js
+++ VIEWS/menu_login.js
@@ -2,6 +2,6 @@ LogoLogin = document.getElementById('log');
 menuLog = document.querySelector('form.login');
 
 LogoLogin.onclick= function () {
-    menuLog.classList.remove('menuAppair');
+    menuLog.classList.add('menuAppair');
 
 }
