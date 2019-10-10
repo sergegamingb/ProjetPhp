f669904ad0a71d5b91437946f028bb3f6e6830ee
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-10 12:13:46 +0200

    test du login

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 
     <ul>
-        <li> <a href="VIEWS/inscription.html" > inscription </a> </li>
+        <li> <a href="VIEWS/inscription.html" > inscription. </a> </li>
        <li> <a href="VIEWS/view_index.html" > login </a>   </li>
         <li> <a href="VIEWS/view_test.php"  </li>
     </ul>
