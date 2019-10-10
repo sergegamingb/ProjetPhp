565b89f8f7e07dddf0387695927dd84a7f67a5f8
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-10 17:33:23 +0200

    test menu

--- VIEWS/menu_login.js
+++ VIEWS/menu_login.js
@@ -3,8 +3,10 @@ menuLog = document.querySelector('.loginmenu');
 bod = document.getElementById('1');
 
 LogoLogin.onclick = function () {
-        menuLog.classList.toggle("loginmenunoff");
-        bod.style.background="red";
+
+        alert('hachek');
+        //        menuLog.classList.toggle("loginmenunoff");
+ //       bod.style.background="red";
 }
 
 bodyy = document.getElementById("2");
