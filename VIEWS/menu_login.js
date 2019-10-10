8bf97eedac76892918884859ba5957ea73646fa9
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-10 16:43:02 +0200

    test menu

--- VIEWS/menu_login.js
+++ VIEWS/menu_login.js
@@ -1,15 +1,8 @@
 LogoLogin = document.getElementById("logo");
 menuLog = document.querySelector('.loginmenu');
-Vmenu = document.querySelector('.menuV');
-bout = document.getElementById('bouton');
 
-
-LogoLogin.onclick = function  clique() {
+function clique() {
         menuLog.classList.toggle("loginmenunoff");
-}
-
-bout.onclick = function () {
-        Vmenu.classList.toggle("menuVoff");
 }
 
 
