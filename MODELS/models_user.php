0af72f1b388ed3c6d1e528936dfef6b3e03fe145
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-10 14:51:44 +0200

    test login

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -253,31 +253,52 @@ class User
 
         $login = $_POST['login'];
         $password = $_POST['mdp'];
-        $dbLink = $GLOBALS['dbLink'];
+        $db = $GLOBALS['db'];
+
+
+
+
+
 
 
         $query = "SELECT pseudo , password  FROM USER where USER.pseudo =  '$login'  and USER.password = '$password' ";
-        if (!($dbResult = mysqli_query($dbLink, $query))) {
-            echo 'Erreur de requête<br/>';
-// Affiche le type d'erreur.
-            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-// Affiche la requête envoyée.
-            echo 'Requête : ' . $query . '<br/>';
-            exit();
-        }
-        if($dbRow = mysqli_fetch_assoc($dbResult))
+
+        try
         {
-            $_SESSION['isLogin']='ok';
-            $_SESSION['login']=$login;
-            $_SESSION['password']=$password;
-            header('Location: ../index.php');
+        $db->query($query);
+        $_SESSION['isLogin']='ok';
+        $_SESSION['login']=$login;
+        $_SESSION['password']=$password;
+        header('Location: ../index.php');
+
         }
 
-        else
-        {
-            echo 'erreur d\'authentification';
-            echo '<br/> <a href="../VIEWS/view_index.html"> retourner au login</a> ';
-        }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+
+
+//
+//        if (!($dbResult = mysqli_query($dbLink, $query))) {
+//            echo 'Erreur de requête<br/>';
+//// Affiche le type d'erreur.
+//            echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+//// Affiche la requête envoyée.
+//            echo 'Requête : ' . $query . '<br/>';
+//            exit();
+//        }
+//        if($dbRow = mysqli_fetch_assoc($dbResult))
+//        {
+//
+//        }
+//
+//        else
+//        {
+//            echo 'erreur d\'authentification';
+//            echo '<br/> <a href="../VIEWS/view_index.html"> retourner au login</a> ';
+//        }
     }
 }
 
