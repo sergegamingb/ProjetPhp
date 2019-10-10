67bf8ae241f325efacfca6a54b896bea74318b82
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-10-10 12:02:22 +0200

    PDO

--- CONTROLLERS/controller_user.php
+++ CONTROLLERS/controller_user.php
@@ -2,19 +2,19 @@
 
 include '../MODELS/models_user.php';
 
+
 $action = $_POST['action'];
-$dbLink = $GLOBALS['dbLink'];
-$action = $_POST['action'];
-$email = $_POST['mail'];
+$mail = $_POST['mail'];
 $password = $_POST['mdp'];
-$identifiant = $_POST['identifiant'];
-$telephone = $_POST['phone'];
-$pays = $_POST['pays'];
-$conditions = 'member';
+$pseudo = $_POST['identifiant'];
+$gender = $_POST['genre'];
+$phone = $_POST['phone'];
+$country = $_POST['pays'];
+$state = 'member';
 
 if($action =='inscription')
 {
 
-    $user = new User;
+    $user = new User($mail, $password, $pseudo, $gender, $phone, $country, $state);
     $user->register();
 }
