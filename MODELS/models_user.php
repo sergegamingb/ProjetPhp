b51c4c34004cc7ca8bb97543a464883c5937fdf5
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-15 19:03:26 +0200

    securité formulaire

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -276,9 +276,9 @@ class User extends base
 //        $sql->bindValue(':password', $hashedPass, PDO::PARAM_STR);
 //
         //$result = $sql->fetchAll();
-//        $sql = $this->loadDb()->prepare("SELECT * FROM USER WHERE  pseudo = ? and password = ?");
-//        $sql->execute(array($login, $hashedPass));
-        $query = 'SELECT pseudo , password  FROM USER where USER.pseudo =  \'' . $login . '\'  and USER.password = \'' . $hashedPass .'\' ';
+        $sql = $this->loadDb()->prepare("SELECT * FROM USER WHERE  pseudo = ? and password = ?");
+        $sql->execute(array($login, $hashedPass));
+//        $query = 'SELECT pseudo , password  FROM USER where USER.pseudo =  \'' . $login . '\'  and USER.password = \'' . $hashedPass .'\' ';
 
         if(!preg_match('#^[a-zA-Z0-9_]*$#', $login))
         {
@@ -289,7 +289,7 @@ class User extends base
 
         try
         {
-        $row = $this->execRequete($query);
+        $row = $this->execute($sql);
         if($row -> rowCount() == 0)
         {
             echo '<br/><strong>erreur d\'authentification</strong><br/>';
