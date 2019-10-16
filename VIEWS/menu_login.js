42eb1c76e2b78786c2f9f2d282b7b66bd2f442a3
VincentSD <vincentsd@yahoo.com>
2019-10-16 02:36:43 +0200

    test page d'accueil

--- VIEWS/menu_login.js
+++ VIEWS/menu_login.js
@@ -1,14 +1,9 @@
 LogoLogin = document.querySelector(".coin");
 menuLog = document.querySelector('.loginmenu');
-bod = document.getElementById('1');
 
 LogoLogin.onclick = function () {
 
     menuLog.classList.toggle("loginmenunoff");
 }
 
-bodyy = document.getElementById("2");
-function clique() {
-        bodyy.style.backgroundColor="red";
-}
 
