f6af841deed59e3572754082c38df45c0c27dc3d
VincentSD <vincentsd@yahoo.com>
2019-10-17 00:36:13 +0200

    page perso

--- VIEWS/profilePage.php
+++ VIEWS/profilePage.php
@@ -11,7 +11,7 @@ $user=$_SESSION['user'];
 <div class="container">
     <table class="col-lg-12">
 
-        <tr> <td> Pseudo : <?php echo $_SESSION['login'];  ?> </td> </tr>
+        <tr> <td> Pseudo : <?php echo $user->getLogin();  ?> </td> </tr>
         <tr> <td> <a href="view_changePass.html"> Changer mot de passe  </a>          </td>  </tr>
         <tr> <td> Email :  <?php echo  $_SESSION['mail'];?>       </td> </tr>
         <tr> <td> Numéro : <?php echo  $_SESSION['phone']; ?>    </td> </tr>
