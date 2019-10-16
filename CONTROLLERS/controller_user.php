ba8e49e8d85ef4394c3fd389dcbc83f488845423
VincentSD <vincentsd@yahoo.com>
2019-10-17 00:41:03 +0200

    page perso

--- CONTROLLERS/controller_user.php
+++ CONTROLLERS/controller_user.php
@@ -1,13 +1,11 @@
 <?php
-
+session_start();
 spl_autoload_register(function ($class_name) {
     include  '../MODELS/models_' . $class_name . '.php';
 });
 
-
 $action = $_POST['action'];
 
-session_start();
 $user=new user;
 $_SESSION['user']=$user;
 
