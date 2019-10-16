036c48a8a5210083fd5f2836b1316582be0b7b50
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-16 10:58:27 +0200

    accueil

---
+++ VIEWS/profilePage.php
@@ -0,0 +1,12 @@
+<?php $title ='Profil'; ?>
+
+<?php ob_start(); ?>
+
+<p>coucou </p>
+
+
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
--- VIEWS/template.php
+++ VIEWS/template.php
@@ -3,20 +3,69 @@
 <head>
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="style.css" />
-    <title><?= $titre ?></title>   <!-- Élément spécifique -->
+    <title><?= $title ?></title>   <!-- Élément spécifique -->
 </head>
 <body>
 <div id="global">
-    <header>
-        <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
-        <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
-    </header>
-    <div id="contenu">
-        <?= $contenu ?>   <!-- Élément spécifique -->
+    <div class="container">
+
+        <header class="row">
+            <div class="col-xs-2 col-lg-2 text-center"> logo </div>
+            <div class="col-xs-7 col-lg-7 text-center"> <h1>FreeNote</h1> </div>
+            <div class="col-xs-2 col-lg-2 text-center" >
+                <a class="coin" > se connecter </a>
+
+                <form class="loginmenu" action="../CONTROLLERS/controller_user.php" method="post" >
+                    <input type="text" name="login"  placeholder="Identifiant"/>
+                    <input type="password" name="mdp" placeholder="Mot de passe"/>
+                    <input type="submit" name="action" value="login"/>
+                </form>
+                <script src="menu_login.js"></script>
+            </div >
+
+            <div class="col-xs-1 col-lg-1 text-center">
+                <a class="reg"> s'inscrire </a>
+
+                <form class="registerMenu" action="../CONTROLLERS/controller_user.php" method="post">
+                    <input type="text" placeholder="identifiant" name="identifiant" /> <br>
+                    <label> sexe </label> <br>
+                    <input type="radio" value="homme" name="genre" checked/> homme <br>
+                    <input type="radio" value="femme" name="genre" checked/> femme <br>
+                    <input type="text" placeholder="mail" name="mail"/>   <br>
+                    <input type="password" placeholder="mot de passe" name="mdp"/> <br>
+                    <input type="password" placeholder="confirmer mot de passe" name="cmdp"/> <br>
+                    <input type="text" placeholder="telephone" name="phone" /> <br>
+                    <select  name="pays">
+                        <option> france </option>
+                        <option> espagne </option>
+                        <option> nigeria</option>
+                        <option> afgranistan </option>
+                    </select> <br>
+                    <label> conditions generales </label>
+                    <input type="checkbox" name="conditions"/> <br>
+                    <input type="submit" name="action" value="inscription"/> <br>
+                </form>
+                <script src="menu_login.js"></script>
+
+            </div>
+        </header>
     </div>
-    <footer id="piedBlog">
-        Blog réalisé avec PHP, HTML5 et CSS.
+    <div id="contenu">
+        <?= $content ?>   <!-- Élément spécifique -->
+    </div>
+
+    <footer class="row" >
+
+
+        <div class ="col-lg-12 text-center"> Copyright | FreeNote </div>
+        <div class="col-lg-3 text-center"> <a> Vincent Simonetti-Diez </a> </div>
+        <div class="col-lg-3 text-center"> <a> Alexandre Salles  </a> </div>
+        <div class="col-lg-3 text-center"> <a> Ibrahim Bouhlarouz </a> </div>
+        <div class="col-lg-3 text-center"> <a>  Anthony Ziane </a> </div>
+
+
     </footer>
+
 </div> <!-- #global -->
 </body>
 </html>
