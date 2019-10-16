a36b1f195b1f84f2d93e9492cf8f1beb1e06bd7e
Ziane Anthony <ant>
2019-10-16 19:24:04 +0200

    html/css index

--- VIEWS/accueil.php
+++ VIEWS/accueil.php
@@ -9,6 +9,9 @@ session_start();
 
 <hr>
 <div class="container">
+    <div class="bbloc">
+        <p class="longtext"> Bienvenue sur FreeNote, le nouveau réseau social. Comme ses concurrents, FreeNote est basé sur la communication entre utilisateurs autour de discussions. Mais ce dernier se démarque des autres car les discussions sont limités, chaque utilisateur peut ajouter au maximum deux mots ce qui permet la création de discussions assez surprenante. Le divertissement est donc la seconde valeur du site, l'amusement est donc fortement possible.En revanche, en cas d'un propos injurieux des mesures de grandes ampleurs seront prises par nous, les administrateurs. La politesse et la courtoisie sont donc essentiels. On vous souhaite de vous amusez ! </p>
+    </div>
         <section class="col-lg-12">
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-sm-12 text-center"><input class="newbouton" type="button" value="Nouvelle Discussion"> </div>
