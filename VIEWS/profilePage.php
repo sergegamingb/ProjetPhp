07f2589d7e41e8cace89a69fa9611f560483887a
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-16 11:32:00 +0200

    profil

--- VIEWS/profilePage.php
+++ VIEWS/profilePage.php
@@ -7,18 +7,19 @@ session_start();
 
 ?>
 
+<hr>
 
 <table class="col-lg-12">
 
     <tr> <td> Pseudo : <?php $_SESSION['login']  ?> </td> </tr>
     <tr> <td> Changer mot de passe </td> </tr>
-    <tr> <td> </td>Email  <?php $_SESSION['mail']?> </tr> </tr>
+    <td> <td> </td>Email  <?php $_SESSION['mail']?> </td> </tr>
     <tr> <td> Numéro  <?php $_SESSION['phone'] ?> </td> </tr>
     <tr> <td> Pays <?php $_SESSION['country']?> </td> </tr>
 
 </table>
 
-
+<hr>
 
 
 <?php $content = ob_get_clean(); ?>
