7a3974da662f10270d5bd0bf160ac5ae74b1d252
VincentSD <vincentsd@yahoo.com>
2019-10-16 03:05:00 +0200

    test page d'accueil

--- VIEWS/menu_login.js
+++ VIEWS/menu_login.js
@@ -7,11 +7,11 @@ RegisterLog = document.querySelector('.registerMenu');
 Login.onclick = function () {
 
     menuLog.classList.toggle("loginmenunoff");
-    RegisterLog.classList.toggle("registerMenu");
+    RegisterLog.classList.toggle("registerMenu" , true );
 
 }
 
 Register.onclick = function () {
     RegisterLog.classList.toggle("registerMenuOff");
-    menuLog.classList.toggle("loginmenu");
+    menuLog.classList.toggle("loginmenu",true);
 }
