3d78f2377efcc044dc4ead3f1203eb00f0f20d3d
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-10-16 10:17:17 +0200

    html accueil

--- VIEWS/template2.php
+++ VIEWS/template2.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title><?= $title ?></title>
     <link href="style2.css" rel="stylesheet" />
-    <link rel="icon" type="image/png" href="images.png" />
+    <link rel="icon" type="image/png" href="issuu-logo-symbol-black.jpg" />
 </head>
 
 <body>
