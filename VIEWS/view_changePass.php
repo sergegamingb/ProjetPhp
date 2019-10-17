9964dbde733234b8f7a9c8d3de5e6b321cd6639f
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-10-17 09:38:20 +0200

    page changement mdp

--- VIEWS/view_changePass.php
+++ VIEWS/view_changePass.php
@@ -6,11 +6,11 @@
 
 
      <div class="changebloc">
-    <form action="../CONTROLLERS/controller_user.php" method="post">
+    <form class="changebloc"  action="../CONTROLLERS/controller_user.php" method="post">
         <input type="password" placeholder="ancien mot de passe" name="oldMdp" /> <br>
         <input type="password" placeholder="nouveau mot de passe" name="newMdp"/> <br>
         <input type="password" placeholder="confirmer mot de passe" name="confirmMdp"/> <br>
-        <input class="modif" type="submit" name="action" value="modifier"/> <br>
+        <input type="submit" name="action" value="modifier"/> <br>
     </form>
      </div>
 
