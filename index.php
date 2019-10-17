bf29cd34f7b71862913aec13b0c741388510981d
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-17 08:45:59 +0200

    page perso

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <li> <a href="VIEWS/view_login.html" > login </a>   </li>
     <li> <a href="VIEWS/view_test.php"> test login </a>  </li>
     <li> <a href="MODELS/logout.php">  se deconnecter </a> </li>
-    <li> <a href="VIEWS/view_changePass.html"> changer mot de passe </a> </li>
+    <li> <a href="VIEWS/view_changePass.php"> changer mot de passe </a> </li>
     <li> <a href="VIEWS/view_accueil.php" > page d'accueil </a> </li>
 </ul>
 
